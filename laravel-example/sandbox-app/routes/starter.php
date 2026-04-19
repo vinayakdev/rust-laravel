994c430f8114f290d9be3d704d805b1099017d5d
hotdogb <v4vinayakdev@gmail.com>
2026-04-19 16:20:53 +0530

    nice

---
+++ laravel-example/sandbox-app/routes/starter.php
@@ -0,0 +1,6 @@
+<?php
+
+Route::get('/', [WebsiteController::class, 'home'])->name('home');
+Route::get('/about', [WebsiteController::class, 'about'])->name('about');
+Route::get('/contact', [WebsiteController::class, 'contact'])->name('contact');
+Route::get('/sustainability', [WebsiteController::class, 'sustainability'])->name('sustainability');
--- laravel-example/sandbox-app/routes/web.php
+++ laravel-example/sandbox-app/routes/web.php
@@ -6,14 +6,20 @@ Route::get('/', function () {
     return 'sandbox';
 })->name('sandbox.home');
 
-Route::prefix('labs')->middleware('web')->group(function () {
-    Route::get('/ping', function () {
-        return 'pong';
-    })->name('labs.ping');
-});
+Route::prefix('labs')
+    ->middleware('web')
+    ->group(function () {
+        Route::get('/ping', function () {
+            return 'pong';
+        })->name('labs.ping');
+    });
 
-Route::prefix('admin')->middleware('admin')->group(function () {
-    Route::get('/products/{slug}', function (string $slug) {
-        return $slug;
-    })->middleware('auth')->name('admin.products.show');
-});
+Route::prefix('admin')
+    ->middleware('admin')
+    ->group(function () {
+        Route::get('/products/{slug}', function (string $slug) {
+            return $slug;
+        })->middleware('auth')->name('admin.products.show');
+    });
+
+require __DIR__ . '/starter.php';
